76d51515e1d3cd16fb2f0069c56749435f093539
esosaoh <esosaohangbon68@gmail.com>
2026-07-17 22:52:44 -0400

    Add ANSI color helpers for terminal output

---
+++ cmd/color.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/esosaoh/dodo/internal/classify"
+)
+
+const (
+	colorReset   = "\033[0m"
+	colorRed     = "\033[31m"
+	colorGreen   = "\033[32m"
+	colorYellow  = "\033[33m"
+	colorMagenta = "\033[35m"
+	colorCyan    = "\033[36m"
+	colorDim     = "\033[2m"
+	colorBold    = "\033[1m"
+)
+
+var colorEnabled = os.Getenv("NO_COLOR") == "" && isTerminal(os.Stdout)
+
+func isTerminal(f *os.File) bool {
+	info, err := f.Stat()
+	return err == nil && info.Mode()&os.ModeCharDevice != 0
+}
+
+func colorize(code, s string) string {
+	if !colorEnabled {
+		return s
+	}
+	return code + s + colorReset
+}
+
+var classColor = map[classify.Class]string{
+	classify.ClassDead:      colorRed,
+	classify.ClassSoft404:   colorMagenta,
+	classify.ClassMalformed: colorYellow,
+	classify.ClassBlocked:   colorCyan,
+	classify.ClassUnknown:   colorDim,
+	classify.ClassAlive:     colorGreen,
+}
